3b91c726c804f927711c9865d5ae01a16c317d26
zazadograu <126686379+zazadograu@users.noreply.github.com>
2024-09-23 08:16:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,37 +9,56 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer."
+                afirmacao: [
+                    "No início ficou com medo do que essa tecnologia pode fazer.",
+                    ""
+            ]
             },
             {
                 texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                afirmacao:[ 
+                    "Quis saber como usar IA no seu dia a dia.",
+                    ""
+            ]
             }           
             
         ]
-        alternativas: [
-            {
-                texto: "Isso é assustador!",
-                afirmacao: [
-                    "No início ficou com medo do que essa tecnologia pode fazer.",
-                    "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
-                ]
-            },
-            {
-                texto: "Isso é maravilhoso!",
-                afirmacao: [
-                    "Quis saber como usar IA no seu dia a dia.",
-                    "Pensou que IA pode ajudar em tarefas da sua vida."
-                ]
-            }
+alternativas: [
+    {
+        texto: "Isso é assustador!",
+        afirmacao: [
+            "No início ficou com medo do que essa tecnologia pode fazer.",
+            "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
         ]
-        
-
-        function aleatorio(lista) {
-            const posicao = Math.random()* lista.length;
+    },
+    {
+        texto: "Isso é maravilhoso!",
+        afirmacao: [
+            "Quis saber como usar IA no seu dia a dia.",
+            "Pensou que IA pode ajudar em tarefas da sua vida."
+        ]
     }
-    function aleatorio (lista){
+]
+function aleatorio(lista) {
+        const posicao = Math.random()* lista.length;
+}
+
+function aleatorio (lista){
         const posicao = Math.floor(Math.random()* lista.length);
         return lista[posicao];
 }
-    
+
+function respostaSelecionada(opcaoSelecionada){
+        const afirmacoes = opcaoSelecionada.afirmacao;
+        historiaFinal += afirmacoes + " ";
+        atual++;
+        mostraPergunta();
+}
+
+function respostaSelecionada(opcaoSelecionada){
+        const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+        historiaFinal += afirmacoes + " ";
+        atual++;
+        mostraPergunta();
+}
+
