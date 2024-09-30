9d1bc54b64b2a015131d3a21b6b923c49f0a80dc
zazadograu <126686379+zazadograu@users.noreply.github.com>
2024-09-30 08:05:46 -0300

    .

---
+++ dadosD6.js
@@ -0,0 +1,12 @@
+function dadoD6() {
+    var numero = Math.floor(Math.random() * 6);
+    return numero;
+}
+
+console.log(dadoD6());
+function dadoD6() {
+    var numero = Math.floor(Math.random() * 6) + 1;
+    return numero;
+}
+
+console.log(dadoD6());
